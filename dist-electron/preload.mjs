9af5a1d6a4f8b776fa7087ed99fbfe335fdeec58
AveryOn <lnkonion2@gmail.com>
2025-02-22 22:35:49 +0300

    fix added cascade delete for bloks

--- dist-electron/preload.mjs
+++ dist-electron/preload.mjs
@@ -1,1 +1,45 @@
-"use strict";const t=require("electron");function c(e){localStorage.clear(),(e==null?void 0:e.fromServer)===!0&&window.location.reload()}function n(e){localStorage.setItem("token",e)}t.contextBridge.exposeInMainWorld("electron",{checkAccess:()=>t.ipcRenderer.invoke("check-access"),validateAccessToken:e=>t.ipcRenderer.invoke("validate-access-token",e),prepareUserStore:e=>t.ipcRenderer.invoke("prepare-user-store",e),getUsers:e=>t.ipcRenderer.invoke("get-users",e),createUser:e=>t.ipcRenderer.invoke("create-user",e),loginUser:e=>t.ipcRenderer.invoke("login-user",e),updatePassword:e=>t.ipcRenderer.invoke("update-password",e),createChapter:(e,r)=>t.ipcRenderer.invoke("create-chapter",e,r),getChapters:(e,r)=>t.ipcRenderer.invoke("get-menu-chapters",e,r),getChapter:(e,r)=>t.ipcRenderer.invoke("get-one-chapter",e,r),createSubChapter:(e,r)=>t.ipcRenderer.invoke("create-sub-chapter",e,r),syncMaterials:e=>t.ipcRenderer.invoke("sync-materials",e),getOneSubChapter:(e,r)=>t.ipcRenderer.invoke("get-one-sub-chapter",e,r),editChapter:(e,r)=>t.ipcRenderer.invoke("edit-chapter",e,r),deleteChapter:(e,r)=>t.ipcRenderer.invoke("delete-chapter",e,r),deleteSubChapter:(e,r)=>t.ipcRenderer.invoke("delete-sub-chapter",e,r),getChapterBlocks:(e,r)=>t.ipcRenderer.invoke("get-chapter-blocks",e,r),getSubChapterBlocks:(e,r)=>t.ipcRenderer.invoke("get-sub-chapter-blocks",e,r),createChapterBlock:(e,r)=>t.ipcRenderer.invoke("create-chapter-block",e,r),editChapterBlock:(e,r)=>t.ipcRenderer.invoke("edit-chapter-block",e,r),deleteChapterBlock:(e,r)=>t.ipcRenderer.invoke("delete-chapter-block",e,r)});t.ipcRenderer.on("logout",(e,r)=>{c(r)});t.ipcRenderer.on("refresh-token",(e,r)=>{n(r)});
+"use strict";
+const electron = require("electron");
+function logout(config) {
+  localStorage.clear();
+  if ((config == null ? void 0 : config.fromServer) === true) {
+    window.location.reload();
+  }
+}
+function refreshToken(token) {
+  localStorage.setItem("token", token);
+}
+electron.contextBridge.exposeInMainWorld("electron", {
+  // ================= SYSTEM  ==================
+  checkAccess: () => electron.ipcRenderer.invoke("check-access"),
+  // ============= AUTH =============
+  validateAccessToken: (params) => electron.ipcRenderer.invoke("validate-access-token", params),
+  // ============= USERS =============
+  prepareUserStore: (params) => electron.ipcRenderer.invoke("prepare-user-store", params),
+  getUsers: (config) => electron.ipcRenderer.invoke("get-users", config),
+  createUser: (params) => electron.ipcRenderer.invoke("create-user", params),
+  loginUser: (params) => electron.ipcRenderer.invoke("login-user", params),
+  updatePassword: (params) => electron.ipcRenderer.invoke("update-password", params),
+  // ===== MATERIALS ========
+  createChapter: (params, auth) => electron.ipcRenderer.invoke("create-chapter", params, auth),
+  getChapters: (params, auth) => electron.ipcRenderer.invoke("get-menu-chapters", params, auth),
+  getChapter: (params, auth) => electron.ipcRenderer.invoke("get-one-chapter", params, auth),
+  createSubChapter: (params, auth) => electron.ipcRenderer.invoke("create-sub-chapter", params, auth),
+  syncMaterials: (auth) => electron.ipcRenderer.invoke("sync-materials", auth),
+  getOneSubChapter: (params, auth) => electron.ipcRenderer.invoke("get-one-sub-chapter", params, auth),
+  editChapter: (params, auth) => electron.ipcRenderer.invoke("edit-chapter", params, auth),
+  deleteChapter: (params, auth) => electron.ipcRenderer.invoke("delete-chapter", params, auth),
+  deleteSubChapter: (params, auth) => electron.ipcRenderer.invoke("delete-sub-chapter", params, auth),
+  // ======= MATERIALS > BLOCKS ========
+  getChapterBlocks: (params, auth) => electron.ipcRenderer.invoke("get-chapter-blocks", params, auth),
+  getSubChapterBlocks: (params, auth) => electron.ipcRenderer.invoke("get-sub-chapter-blocks", params, auth),
+  createChapterBlock: (params, auth) => electron.ipcRenderer.invoke("create-chapter-block", params, auth),
+  editChapterBlock: (params, auth) => electron.ipcRenderer.invoke("edit-chapter-block", params, auth),
+  deleteChapterBlock: (params, auth) => electron.ipcRenderer.invoke("delete-chapter-block", params, auth)
+});
+electron.ipcRenderer.on("logout", (_, config) => {
+  logout(config);
+});
+electron.ipcRenderer.on("refresh-token", (_, token) => {
+  refreshToken(token);
+});
